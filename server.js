238d67e083a1f4a8c6753e0b81b43f4541852246
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-13 22:45:20 -0500

    Creating db and tables in schema.sql

--- server.js
+++ server.js
@@ -21,3 +21,12 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the staff_db database.`)
 );
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
